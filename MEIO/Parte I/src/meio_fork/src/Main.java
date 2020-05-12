6488d1f04916221bedbfac16e74e38454b2a7575
EtienneCosta <etienne_costa@hotmail.com>
2020-05-12 04:17:03 +0100

    Projecto de MEIO

---
+++ MEIO/Parte I/src/meio_fork/src/Main.java
@@ -0,0 +1,35 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        /* calculate odds and costs */
+        Double[][] probB1 = Odds.deliveryRequestProbs(Odds.b1);
+        Double[][] probB2 = Odds.deliveryRequestProbs(Odds.b2);
+
+        Double[][] costB1 = Odds.costsProbs(Odds.b1);
+        Double[][] costB2 = Odds.costsProbs(Odds.b2);
+
+        /* calculate all the decisions */
+        Double[][][][] detachedDecisions = Odds.allDecisions(probB1,probB2,'P');
+        Double[][][] decisions = Odds.mergeDecisions(detachedDecisions, 'P');
+
+        /* calculate all the costs */
+        Double[][][][] detachedCosts = Odds.allDecisions(costB1,costB2,'C');
+        Odds.transferCost(detachedCosts);
+        Double[][][] costs = Odds.mergeDecisions(detachedCosts,'C');
+
+        /* calculates de best policy */
+        Policy policy = Calculate.policy(decisions,costs);
+
+        /* prints the ebst policy */
+
+        System.out.println("Optimal policy = " + policy.getGain());
+        Print.printVector(policy.getDecisions(), 169);
+        //Export.dumpMatricesIntoCSV(detachedDecisions, detachedCosts);
+
+        /*Double[][] c = detachedDecisions[0][1];
+        Print.printSumLines(c,Odds.states,Odds.states);
+        Print.printMatrix(c,Odds.states,Odds.states);*/
+    }
+}
